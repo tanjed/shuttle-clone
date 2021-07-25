824faac7771f088ab30fd96bc67d136a0e555f46
HabibTanoy <hbtonoy42@gmail.com>
2021-07-25 14:29:20 +0600

    update section and image show

--- resources/views/client/includes/header.blade.php
+++ resources/views/client/includes/header.blade.php
@@ -11,19 +11,22 @@
     </div>
     <!--contact and email-->
     <main>
-        <div>
+        <!-- <div> -->
             <div class="container-fluid">
                 <div class="row justfy-content-center">
                     <nav class="navbar navbar-expand-md navbar-light shadow-sm">
-                        <div class="container-fluid" id="for_logo">
-                            <a href="{{route('client.show.home')}}">
-                                <img src="{{asset('client/image/shuttle-logo.png')}}" class="navbar-brand" style="cursor: pointer;" alt="">
-                            </a>
-                            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                      <span>
-                        <i class="fas fa-bars"></i>
-                      </span>
-                            </button>
+                    <a class="navbar-brand" href="{{route('client.show.home')}}" id="for_logo">
+                        <img src="{{asset('client/image/shuttle-logo.png')}}" width="30" height="30" alt="">
+                    </a>
+                        <!-- <div class="container-fluid" > -->
+                                <!-- <a href="{{route('client.show.home')}}"> -->
+                                    <!-- <img src="{{asset('client/image/shuttle-logo.png')}}" class="navbar-brand" style="cursor: pointer;" alt=""> -->
+                                <!-- </a> -->
+                                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                                <span>
+                                    <i class="fas fa-bars"></i>
+                                </span>
+                                </button>
                             <!--for burger-->
                             <div class="navbar-collapse collapse" id="navbarSupportedContent">
                                 <!--left side of navbar-->
@@ -44,96 +47,9 @@
                                     </li>
                                 </ul>
                             </div>
-                        </div>
+                        <!-- </div> -->
                     </nav>
                     <!--End of navbar-->
-                    <div id="header-section">
-                        <div class="col-md-12">
-                            <div class="row">
-                                <div class="col-md-8 col-lg-8 header-title">
-                                    <h2>Sign up to Shuttle for Business</h2>
-                                    <h3>Ensure safe transportation for your moving employees in lockdown</h3>
-                                    <a href="" type="button" id="land-signup" class="btn btn-info" data-toggle="modal" data-target="#exampleModalCenter" style="-webkit-appearance: none;">sign up</a>
-                                    <!-- Modal -->
-                                    <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-                                        <div class="modal-dialog modal-dialog-centered" role="document">
-                                            <div class="modal-content">
-                                                <div class="modal-header text-center" style="border:none;">
-                                                    <div class="modal-title w-100 font-weight-bold">
-                                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                                            <span aria-hidden="true" style="color: #dee2e6;">&times;</span>
-                                                        </button>
-                                                        <h4 class="" id="exampleModalLongTitle">Want a customized solution for your organization?</h4>
-                                                        <p>Add your company name, contact information below and our team will get in touch with you</p>
-                                                    </div>
-                                                </div>
-                                                <div class="modal-body mx-3">
-                                                    <div class="container-fluid">
-                                                        <div class="row justfy-content-center">
-                                                            <div class="col-md-12">
-                                                                <form action="">
-                                                                    <div class="form-group row">
-                                                                        <div class="col-md-12">
-                                                                            <input type="text" class="form-control" name="" placeholder="Company Name" required autofocus>
-                                                                        </div>
-                                                                    </div>
-                                                                    <div class="form-group row">
-                                                                        <div class="col-md-12">
-                                                                            <input type="text" class="form-control" name="" placeholder="Contact Name" required>
-                                                                        </div>
-                                                                    </div>
-                                                                    <div class="form-group row">
-                                                                        <div class="col-md-6" id="email">
-                                                                            <input type="email" class="form-control" name="" placeholder="Email" required>
-                                                                        </div>
-                                                                        <div class="col-md-6">
-                                                                            <input type="text" class="form-control" name="" placeholder="Phone Number" required>
-                                                                        </div>
-                                                                    </div>
-                                                                    <div class="form-group row mb-0 pt-3">
-                                                                        <div class="col-md-6 offset-md-4 for_btn">
-                                                                            <button style="border-radius: 8px" type="submit" class="btn btn-sm btn-info">Submit</button>
-                                                                        </div>
-                                                                    </div>
-                                                                </form>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <!--end of modal-->
-                                </div>
-                                <div class="col-md-4 col-lg-4">
-                                    <div id="social-contact">
-                                        <ul>
-                                            <li>
-                                                <a target="_blank" href="">
-                                                    <i class="fab fa-facebook-f"></i>
-                                                </a>
-                                            </li>
-                                            <li>
-                                                <a target="_blank" href="">
-                                                    <i class="fab fa-twitter"></i>
-                                                </a>
-                                            </li>
-                                            <li>
-                                                <a target="_blank" href="">
-                                                    <i class="fab fa-linkedin"></i>
-                                                </a>
-                                            </li>
-                                            <li>
-                                                <a target="_blank" href="">
-                                                    <i class="fab fa-instagram"></i>
-                                                </a>
-                                            </li>
-                                        </ul>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
                 </div>
             </div>
         </div>
--- resources/views/client/pages/business.blade.php
+++ resources/views/client/pages/business.blade.php
@@ -1,6 +1,137 @@
 @extends('client.master')
 @section('title','Business')
 @section('content')
+<div id="header-section">
+                        <div class="col-md-12">
+                            <div class="row">
+                                <div class="col-md-8 col-lg-8 header-title">
+                                    <h2>Sign up to Shuttle for Business</h2>
+                                    <h3>Ensure safe transportation for your moving employees in lockdown</h3>
+                                    <a href="" type="button" id="land-signup" class="btn btn-info" data-toggle="modal" data-target="#exampleModalCenter" style="-webkit-appearance: none;">sign up</a>
+                                    <!-- Modal -->
+                                    <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                                        <div class="modal-dialog modal-dialog-centered" role="document">
+                                            <div class="modal-content">
+                                                <div class="modal-header text-center" style="border:none;">
+                                                    <div class="modal-title w-100 font-weight-bold">
+                                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                            <span aria-hidden="true" style="color: #dee2e6;">&times;</span>
+                                                        </button>
+                                                        <h4 class="" id="exampleModalLongTitle">Want a customized solution for your organization?</h4>
+                                                        <p>Add your company name, contact information below and our team will get in touch with you</p>
+                                                    </div>
+                                                </div>
+                                                <div class="modal-body mx-3">
+                                                    <div class="container-fluid">
+                                                        <div class="row justfy-content-center">
+                                                            <div class="col-md-12">
+                                                                <form action="">
+                                                                    <div class="form-group row">
+                                                                        <div class="col-md-12">
+                                                                            <input type="text" class="form-control" name="" placeholder="Company Name" required autofocus>
+                                                                        </div>
+                                                                    </div>
+                                                                    <div class="form-group row">
+                                                                        <div class="col-md-12">
+                                                                            <input type="text" class="form-control" name="" placeholder="Contact Name" required>
+                                                                        </div>
+                                                                    </div>
+                                                                    <div class="form-group row">
+                                                                        <div class="col-md-6" id="email">
+                                                                            <input type="email" class="form-control" name="" placeholder="Email" required>
+                                                                        </div>
+                                                                        <div class="col-md-6">
+                                                                            <input type="text" class="form-control" name="" placeholder="Phone Number" required>
+                                                                        </div>
+                                                                    </div>
+                                                                    <div class="form-group row mb-0 pt-3">
+                                                                        <div class="col-md-6 offset-md-4 for_btn">
+                                                                            <button style="border-radius: 8px" type="submit" class="btn btn-sm btn-info">Submit</button>
+                                                                        </div>
+                                                                    </div>
+                                                                </form>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <!--end of modal-->
+                                </div>
+                                <div class="col-md-4 col-lg-4">
+                                    <div id="social-contact">
+                                        <ul>
+                                            <li>
+                                                <a target="_blank" href="">
+                                                    <i class="fab fa-facebook-f"></i>
+                                                </a>
+                                            </li>
+                                            <li>
+                                                <a target="_blank" href="">
+                                                    <i class="fab fa-twitter"></i>
+                                                </a>
+                                            </li>
+                                            <li>
+                                                <a target="_blank" href="">
+                                                    <i class="fab fa-linkedin"></i>
+                                                </a>
+                                            </li>
+                                            <li>
+                                                <a target="_blank" href="">
+                                                    <i class="fab fa-instagram"></i>
+                                                </a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+<div id="feature">
+          <div class="container">
+            <h1 class="my-5 text-center font-weight-bold">Corporate Pick & Drop Services</h1>
+          </div>
+          <div class="container-fluid">
+              <h3 class="text-uppercase text-center mb-3">our difference</h3>
+              <div class="row justfy-content-center" id="child-feature1">
+                  <div class="col-md-4 col-lg-4">
+                      <img src="{{asset('client/image/flexible.jpeg')}}" alt="">
+                      <p class="text-center">Flexible employee pick up & drop off</p>
+                  </div>
+                  <div class="col-md-4 col-lg-4">
+                    <img src="{{asset('client/image/customized-routes.jpeg')}}" alt="">
+                    <p class="text-center">Customized routes</p>
+                </div>
+                <div class="col-md-4 col-lg-4">
+                    <img src="{{asset('client/image/dedicated-manager.jpeg')}}" alt="">
+                    <p class="text-center">Dedicated relationship manager</p>
+                </div>
+              </div>
+              <!--first difference-->
+              <div class="row justfy-content-center" id="child-feature2">
+                <div class="col-md-4 col-lg-4">
+                    <img src="{{asset('client/image/exclusive-app.jpeg')}}" alt="">
+                    <p class="text-center">Exclusive app for the organization</p>
+                </div>
+                <div class="col-md-4 col-lg-4">
+                  <img src="{{asset('client/image/hotline.jpeg')}}" alt="">
+                  <p class="text-center">24/7 hotline support</p>
+              </div>
+              <div class="col-md-4 col-lg-4">
+                  <img src="{{asset('client/image/real-dashboard.jpeg')}}" alt="">
+                  <p class="text-center">Real-time dashboard for Human Resources</p>
+              </div>
+            </div>
+          </div>
+        </div>
+       <!--End of service-->
+        <div id="hygiene">
+          <img id="hygiene-original" src="{{asset('client/image/safety.png')}}" alt="">
+          <img id="hygiene-mobile" width="100%" src="{{asset('client/image/corona-safety-mobile.png')}}" alt="">
+        </div>
+        <!--end of corona pic-->
+      </main>
     <section id="shuttle-business">
         <div class="container-fluid">
             <div class="row justfy-content-center">
